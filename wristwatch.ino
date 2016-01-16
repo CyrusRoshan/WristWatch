6e6954a409aafe5e868328190655db50b2d389f8
Cyrus Roshan <CyrusRoshan@users.noreply.github.com>
2016-01-16 10:56:22 -0600

    added arduino file

---
+++ wristwatch.ino
@@ -0,0 +1,27 @@
+void setup() {
+    pinMode(9, OUTPUT);
+    Serial.begin(9600);
+}
+
+// the loop routine runs over and over again forever:
+void loop() {
+    Serial.print("[");
+    Serial.print(analogRead(A0));
+    Serial.print(", ");
+    Serial.print(analogRead(A1));
+    Serial.print(", ");
+    Serial.print(digitalRead(8));
+    Serial.println("]");
+    delay(10);
+}
+
+void serialEvent() {
+    while (Serial.available()) {
+        char inChar = (char)Serial.read();
+        if (inChar == '0' || inChar == '1') {
+            digitalWrite(9, HIGH);
+        } else {
+            digitalWrite(9, LOW);
+        }
+    }
+}
